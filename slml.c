9a210b0d4fd8ab53f273e4ba33809ded8874cdfb
Bobby Powers <bobbypowers@gmail.com>
2013-10-18 16:31:15 -0400

    read id3 header

--- slml.c
+++ slml.c
@@ -4,6 +4,7 @@
 
 #include <ftw.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,10 +14,23 @@
 
 char *argv0;
 
-static void die(const char *, ...);
+typedef struct {
+	int major; // version
+	int minor; // version
+	bool unsync;
+	bool extended;
+	bool experimental;
+	bool footer;
+	int len;
+} ID3Header;
+
+static void die(char const*, ...);
+static bool is_music_file(char const*);
+static ID3Header *id3_header(FILE*);
+static size_t parse_size(unsigned char const*);
 
 void
-die(const char *fmt, ...)
+die(char const *fmt, ...)
 {
 	va_list args;
 
@@ -27,12 +41,81 @@ die(const char *fmt, ...)
 	exit(EXIT_FAILURE);
 }
 
+bool
+is_music_file(char const *fpath)
+{
+	char *ext;
+	ext = strrchr(fpath, '.');
+	if (!ext)
+		return false;
+	ext++;
+
+	if (strcmp(ext, "mp3") == 0 ||
+	    strcmp(ext, "m4a") == 0 ||
+	    strcmp(ext, "ogg") == 0 ||
+	    strcmp(ext, "flac") == 0)
+		return true;
+	return false;
+}
+
+size_t
+parse_size(unsigned char const*b)
+{
+	size_t n = 0;
+	for (size_t i = 0; i < 4; i++)
+		n |= (b[i] & 0x7f) << (4 - i - 1)*7;
+	return n;
+}
+
+ID3Header*
+id3_header(FILE *f)
+{
+	unsigned char buf[10];
+	ID3Header *h;
+
+	size_t n = fread(buf, 1, 10, f);
+	if (n < 10)
+		goto err;
+
+	if (!(buf[0] == 'I' && buf[1] == 'D' && buf[2] == '3')) {
+		goto err;
+	}
+
+	h = calloc(1, sizeof(*h));
+	h->major = buf[3];
+	h->minor = buf[4];
+	h->unsync = (buf[5] & (1<<7)) != 0;
+	h->extended = (buf[5] & (1<<6)) != 0;
+	h->experimental = (buf[5] & (1<<5)) != 0;
+	h->footer = (buf[5] & (1<<4)) != 0;
+	h->len = parse_size(buf+6);
+
+	return h;
+err:
+	rewind(f);
+	return NULL;
+}
+
 int
-check_entry(const char *fpath, const struct stat *sb, int typeflag,
+check_entry(char const *fpath, const struct stat *sb, int typeflag,
 	    struct FTW *ftwbuf)
 {
-	fprintf(stderr, "p: %s\n", fpath);
 
+	if (!is_music_file(fpath))
+		return 0;
+
+	FILE *f = fopen(fpath, "r");
+	if (!f)
+		return 0;
+
+	ID3Header *h = id3_header(f);
+	if (!h)
+		goto out;
+
+	fprintf(stderr, "'%s': <%d.%d len:%d>\n", fpath, h->major, h->minor, h->len);
+out:
+	free(h);
+	fclose(f);
 	return 0;
 }
 
